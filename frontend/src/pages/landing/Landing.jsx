350ba8de854e20bd59bca9b4c0eb7c9d654595d9
Shreyas <mshreyasviswanath@gmail.com>
2025-07-09 11:05:05 +0530

    Edit Profile for logged in users

--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
 import Profile from "../profile/Profile";
+import { Link } from "react-router-dom";
 import './landing.css'
 const Landing = () => {
     return(
         <>
-
+          <p><Link to="/profile">EDIT PROFILE</Link></p>
         </>
     );
 };
